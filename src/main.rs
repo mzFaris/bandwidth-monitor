5ec6e0f4f7dcacede4313fdc4a7eabfac2beb127
Mirza Faris Al Arifin <alarifinmirzafaris@gmail.com>
2025-12-16 10:56:30 +0700

    get interface to working with

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use pnet::datalink::linux::interfaces;
+
 fn main() {
-    println!("Hello, world!");
+    let interfaces = interfaces();
+    let default_interface = interfaces
+        .iter()
+        .find(|&e| e.is_up() && !e.is_loopback() && !e.ips.is_empty())
+        .expect("no usable interface found");
+    println!("{:#?}", default_interface);
 }
